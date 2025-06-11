269fd2b75bf352d4e854cbf72653ef60f2469387
Nickdubale <nickdubale05@gmail.com>
2025-06-11 20:38:03 +0530

    Back to practice with new file

---
+++ quotesDiff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ echo "here we start with the script"
+ echo "We will define a variable with string value and try to print it inside single and double quotes"
+ name="Swapnil"
+ surname='Dubale'
+ echo 'printing surname with single quotes : $surname'
+ echo "printing name with double quotes : $name"
